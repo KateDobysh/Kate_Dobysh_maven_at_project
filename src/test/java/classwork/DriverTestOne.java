3a14d443b607463f1739fbe2ab68ba5d2a9d0362
Kate Dobysh <katsiaryna.dobysh@gmail.com>
2024-07-24 22:06:11 +0200

    homework_xpath_tasks

---
+++ src/test/java/classwork/DriverTestOne.java
@@ -0,0 +1,15 @@
+package classwork;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DriverTestOne {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://google.com");
+        driver.navigate().to("https://ya.ru");
+        driver.navigate().to("https://google.com");
+        driver.navigate().refresh();
+        driver.quit();
+    }
+}
