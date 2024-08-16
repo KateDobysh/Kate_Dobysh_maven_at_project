60355d452bda60c49fe2c05666dec40dda7aa028
Kate Dobysh <katsiaryna.dobysh@gmail.com>
2024-08-16 19:41:10 +0200

    homework_Cucumber_logger

--- src/test/java/steps/BookingStep.java
+++ src/test/java/steps/BookingStep.java
@@ -17,7 +17,7 @@ public class BookingStep {
     private BookingCurrencyPage bookingCurrencyPage;
 
     @Given("Open booking website")
-    public void openSite() {
+    public void open_booking_website() {
 
         logger.info("Initializing the WebDriver and opening the Booking website");
         driver = Driver.getDriver();
@@ -29,25 +29,25 @@ public class BookingStep {
     }
 
     @When("the currency icon is on the page")
-    public void currencyIconOnThePage() {
+    public void the_currency_icon_is_on_the_page() {
         logger.info("Checking if the currency icon is visible");
         Assert.assertTrue("Currency icon is not visible", bookingCurrencyPage.currencyIconOnThePage());
     }
 
     @Then("I hover over the currency")
-    public void hoverOverTheCurrency() {
+    public void i_hover_over_the_currency() {
         logger.info("Hovering over the currency icon");
         bookingCurrencyPage.hoverOverTheCurrency();
     }
 
     @When("the language icon is on the page")
-    public void languageIconOnThePage() {
+    public void the_language_icon_is_on_the_page() {
         logger.info("Checking if the language icon is visible");
         Assert.assertTrue("Language icon is visible", bookingCurrencyPage.languageIconOnThePage());
     }
 
     @Then("I hover over the language")
-    public void hoverOverTheLanguage(){
+    public void i_hover_over_the_language(){
         logger.info("Hovering over the language icon");
         bookingCurrencyPage.hoverOverTheLanguage();
     }
